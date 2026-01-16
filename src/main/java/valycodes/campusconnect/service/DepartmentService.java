6d1e2ce031f7f07386302584706edcc1ed931149
=Val242 <=ebongvalentine70@gmail.com>
2026-01-17 00:37:53 +0100

    Retrieval of all students and instructors done.

--- src/main/java/valycodes/campusconnect/service/DepartmentService.java
+++ src/main/java/valycodes/campusconnect/service/DepartmentService.java
@@ -4,12 +4,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import valycodes.campusconnect.dto.DepartmentDTORequest;
 
-import valycodes.campusconnect.dto.FacultyDTORequest;
 import valycodes.campusconnect.mapper.DepartmentDTOMapper;
 import valycodes.campusconnect.model.Department;
 import valycodes.campusconnect.model.Faculty;
+import valycodes.campusconnect.model.InstructorProfile;
 import valycodes.campusconnect.repository.DepartmentRepository;
 import valycodes.campusconnect.repository.FacultyRepository;
+import valycodes.campusconnect.repository.InstructorRepository;
 
 import java.util.List;
 
@@ -19,15 +20,17 @@ public class DepartmentService {
 
     private final DepartmentRepository departmentRepository;
     private final FacultyRepository facultyRepository;
+    private final InstructorRepository instructorRepository;
     private final DepartmentDTOMapper departmentDTOMapper;
 
     public DepartmentService(
             DepartmentRepository departmentRepository,
-            FacultyRepository facultyRepository,
+            FacultyRepository facultyRepository, InstructorRepository instructorRepository,
             DepartmentDTOMapper mapper
     ) {
         this.departmentRepository = departmentRepository;
         this.facultyRepository = facultyRepository;
+        this.instructorRepository = instructorRepository;
         this.departmentDTOMapper = mapper;
     }
     public List<DepartmentDTORequest> getAllDepartments(){
@@ -43,7 +46,7 @@ public class DepartmentService {
                         new IllegalStateException("Faculty not found"));
         return departmentDTOMapper.apply(department);
     }
-    public DepartmentDTORequest addNewDepartment(DepartmentDTORequest requestDTO) {
+    public void addNewDepartment(DepartmentDTORequest requestDTO) {
         Faculty faculty = facultyRepository.findFacultyById(requestDTO.facultyId())
                 .orElseThrow(()-> new IllegalStateException("Faculty not Found"));
         Department department = new Department();
@@ -52,7 +55,29 @@ public class DepartmentService {
         department.setFaculty(faculty);
         department.getFaculty().getId();
         Department savedDepartment = departmentRepository.save(department);
-        return departmentDTOMapper.apply(savedDepartment);
+        departmentDTOMapper.apply(savedDepartment);
+    }
+
+    public void assignHeadOfDepartment(Integer departmentId, Integer instructorId){
+       Department department = departmentRepository.findById(departmentId)
+                .orElseThrow(() -> new IllegalStateException(
+                        "Faculty with ID " + departmentId + " does not exist"
+                ));
+        InstructorProfile instructor = instructorRepository.findById(instructorId)
+                .orElseThrow(() -> new IllegalStateException(
+                        "Instructor with ID " + instructorId + " does not exist"
+                ));
+        if (department.getHeadOfDepartment() == null || !department.getHeadOfDepartment().getId().equals(instructorId))
+        //Assign the new dean only if a dean doesn’t exist yet, or the dean is different from the current one.
+        {
+            System.out.println("Assigning dean...");
+            department.setHeadOfDepartment(instructor);
+            departmentRepository.saveAndFlush(department);
+            System.out.println("The new dean of the department " + department.getDepartmentName() + " is " + instructor.getFirstname());
+
+        }
+
+        departmentDTOMapper.apply(department);
     }
 
 
